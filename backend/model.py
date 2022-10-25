02991718ff54ff0a5705d67ea6ef9d68100f47c9
derekdylu <derekdylu@gmail.com>
2022-10-25 20:46:48 +0800

    init

---
+++ backend/model.py
@@ -0,0 +1,83 @@
+from unittest.mock import Base
+from bson import ObjectId
+from pydantic import BaseModel, Field
+from typing import Dict, List, Optional
+from sympy import true
+
+class PyObjectId(ObjectId):
+  @classmethod
+  def __get_validators__(cls):
+    yield cls.validate
+
+  @classmethod
+  def validate(cls, v):
+    if not ObjectId.is_valid(v):
+      raise ValueError("Invalid objectid")
+    return ObjectId(v)
+
+  @classmethod
+  def __modify_schema__(cls, field_schema):
+    field_schema.update(type="string")
+
+class User(BaseModel):
+  id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+  username: str = Field(...)
+  img: str = Field(...)
+  interview: list = Field(...)
+
+  class Config:
+    allow_population_by_field_name = True
+    arbitrary_types_allowed = True
+    json_encoders = {ObjectId: str}
+    schema_extra = {
+      "example": {
+        "username": "derekdylu",
+        "img": "./",
+        "interview": [],
+      }
+    }
+
+def user_helper(user) -> dict:
+  return {
+    "id": user["_id"],
+    "username": user["username"],
+    "img": user["img"],
+    "interview": user["interview"]
+  }
+
+class Interview(BaseModel):
+  id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+  userId: str = Field(...)
+  timestamp: str = Field(...)
+  topic: str = Field(...)
+  score: int = Field(...)
+  big: list[int] = Field(...)
+  note: str = Field(...)
+  link: str = Field(...)
+
+  class Config:
+    allow_population_by_field_name = True
+    arbitrary_types_allowed = True
+    json_encoders = {ObjectId: str}
+    schema_extra = {
+      "example": {
+        "userId": "abc1234567890",
+        "timestamp": "1234567890",
+        "topic": "topic",
+        "score": 0,
+        "big": [1,2,3,4,5],
+        "note": "note",
+        "link": "./"
+      }
+    }
+
+def interview_helper(interview) -> dict:
+  return {
+    "id": interview["_id"],
+    "userId": interview["userId"],
+    "timestamp": interview["timestamp"],
+    "topic": interview["topic"],
+    "score": interview["score"],
+    "big": interview["big"],
+    "note": interview["note"]
+  }
