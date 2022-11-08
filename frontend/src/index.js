9de5819ce42936d768e50b1ddc3573a013e03785
Isabelle1007 <58721990+Isabelle1007@users.noreply.github.com>
2022-11-08 16:57:39 +0800

    minor change

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import ResultCard from './Containers/ResultCard';
 import reportWebVitals from './reportWebVitals';
+import EntireResult from './Components/TestResultCard/EntireResult';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <div className="App">
-      <ResultCard />
+      <EntireResult />
     </div>
   </React.StrictMode>
 );
