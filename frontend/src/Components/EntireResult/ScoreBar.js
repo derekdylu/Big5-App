86c34b331807820366b3da261ecdd79004b5c3d4
Isabelle1007 <58721990+Isabelle1007@users.noreply.github.com>
2022-11-11 20:34:00 +0800

    change progressbar height

--- frontend/src/Components/EntireResult/ScoreBar.js
+++ frontend/src/Components/EntireResult/ScoreBar.js
@@ -9,7 +9,7 @@ const c = ['#4FC1E8', '#AC92EB', '#FFCE54', '#A0D568', '#ED5564']
 const ScoreBar = ({score, id}) => {
     
     const color_name = ['OCEAN_O', 'OCEAN_C', 'OCEAN_E', 'OCEAN_A', 'OCEAN_N']
-    const bar_length = parseInt(score)/100*9
+    const bar_length = parseInt(score)/100*8
     // console.log(bar_length)
 
     const [progress, setProgress] = useState(0);
@@ -21,7 +21,7 @@ const ScoreBar = ({score, id}) => {
         return () => {
           clearInterval(timer);
         };
-      }, []);
+    }, []);
     
     return(
         <Grid 
@@ -33,12 +33,12 @@ const ScoreBar = ({score, id}) => {
             marginBottom = '-16px'
         >
             <Grid 
-                item xs={1}
+                item xs={1.5}
                 container
                 direction="column"
                 justifyContent="center"
             >
-                <p>{alp[id]}</p>
+                <p style = {{marginTop: '0.5em', marginBottom: '0.5em'}}>{alp[id]}</p>
             </Grid>
             <Grid 
                 item xs={bar_length}
@@ -50,11 +50,14 @@ const ScoreBar = ({score, id}) => {
                     variant="determinate" 
                     value={progress} 
                     color = {color_name[id]}
+                    sx={{
+                        height: 10,
+                      }}
                 />
             </Grid>
-            <Grid item xs={9.5 - bar_length} marginLeft = "-10px"/>
+            <Grid item xs={9 - bar_length} marginLeft = "-2vw"/>
             <Grid 
-                item xs={1}
+                item xs={1.8}
             >
                 <p>{score}</p>
             </Grid>
