ab0673b29f40e16e8f2789a836a5b6c19b137f9b
derekdylu <derekdylu@gmail.com>
2022-11-19 09:36:24 +0800

    env

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -1,21 +1,32 @@
-import React, {useState} from 'react'
-import GoogleLogin from 'react-google-login';
+import React, { useState, useEffect } from 'react'
+import { GoogleLogin } from 'react-google-login';
+import { gapi } from 'gapi-script';
 
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-const googleClientId = "278069779564-qfghpg04t9ha3kpoa7k05cpvhv3gi12s.apps.googleusercontent.com"
 
 const Login = () => {
+  const googleClientId = "278069779564-qfghpg04t9ha3kpoa7k05cpvhv3gi12s.apps.googleusercontent.com"
   const [loginData, setLoginData] = useState(
     localStorage.getItem('loginData')
       ? JSON.parse(localStorage.getItem('loginData'))
       : null
   );
 
+  useEffect(() => {
+    const initClient = () => {
+        gapi.auth2.init({
+        clientId: googleClientId,
+        scope: ''
+      });
+    };
+    gapi.load('client:auth2', initClient);
+  });
+
   const handleFailure = (result) => {
-    alert("falure", result);
+    alert("falure: ", result);
   };
 
   const handleLogin = async (googleData) => {
@@ -56,7 +67,11 @@ const Login = () => {
         <div>
           {loginData ? (
             <div>
-              <Typography>You logged in as {loginData.email}</Typography>
+              <Typography color="primary">You logged in as {loginData.email}</Typography>
+              <img src={loginData.imageUrl} alt="user image" />
+              <h3>User Logged in</h3>
+              <p>Name: {loginData.name}</p>
+              <p>Email Address: {loginData.email}</p>
               <Button onClick={handleLogout}>Logout</Button>
             </div>
           ) : (
