16c9a90dbea9dfb22e4374221c4c479af0def7c4
Isabelle1007 <58721990+Isabelle1007@users.noreply.github.com>
2022-11-21 12:50:50 +0800

    api error

--- frontend/src/Containers/Home.js
+++ frontend/src/Containers/Home.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PersonalPage from '../Components/PersonalPage/PersonalPage'
 import Navigation from '../Components/Navigation'
 import Login from '../Components/Login'
+// import axios from '../Axios'
+import axios from 'axios'
 
 const Home = () => {
 
@@ -10,21 +12,37 @@ const Home = () => {
   const name_fake = "Isabelle"
   const picURL_fake = "https://images.pexels.com/photos/20787/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350"
 
+  useEffect( () => {
+    // getUserInfo("6357d9eb8c4455fbfb9d9bd2");
+    getUserInfo();
+  }, []);
+
+  const getUserInfo = async () => {
+    console.log("start GET")
+    // const userData = await axios.get(`http://localhost:8000//user/${id}`);
+    const userData = await axios.get(`http://localhost:8000//users`);
+    console.log("end GET")
+    // console.log("response", userData)
+
+    // const data = await userData.json();
+    // console.log("data", data);
+  };
+
   return (
     <>
       <Navigation />
-      {
+      {/* {
         login ?
           <PersonalPage
             username = {name_fake}
             img = {picURL_fake}
           /> :
           <Login />
-      }
-      {/* <PersonalPage
+      } */}
+      <PersonalPage
         username = {name_fake}
         img = {picURL_fake}
-      /> */}
+      />
     </>
   )
 }
