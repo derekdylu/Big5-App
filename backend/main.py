b39e9cd84721dd856d084635dd89094ca5147a2c
derekdylu <derekdylu@gmail.com>
2022-11-22 17:41:15 +0800

    del bend sso:

--- backend/main.py
+++ backend/main.py
@@ -211,28 +211,3 @@ async def add_interview(file: UploadFile):
 
 if __name__ == "__main__":
     uvicorn.run("app", host="0.0.0.0", port=8000, reload=True)
-
-# ---
-# --- SSO
-# ---
-
-@app.route('/login')
-async def login(request: Request):
-  print("called login")
-  redirect_uri = request.url_for('auth')  # This creates the url for the /auth endpoint
-  return await oauth.google.authorize_redirect(request, redirect_uri)
-
-@app.route('/auth')
-async def auth(request: Request):
-  try:
-    access_token = await oauth.google.authorize_access_token(request)
-  except OAuthError:
-    return RedirectResponse(url='/')
-  user_data = await oauth.google.parse_id_token(request, access_token)
-  request.session['user'] = dict(user_data)
-  return RedirectResponse(url='/')
-
-@app.route('/logout')
-async def logout(request: Request):
-  request.session.pop('user', None)
-  return RedirectResponse(url='/')
