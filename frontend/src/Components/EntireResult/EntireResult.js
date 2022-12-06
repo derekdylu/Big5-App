961e3a29ec9fe74232dc976bd010b5787c8b4ec7
derekdylu <derekdylu@gmail.com>
2022-12-07 01:27:57 +0800

    add some functions about interveiw get and post

--- frontend/src/Components/EntireResult/EntireResult.js
+++ frontend/src/Components/EntireResult/EntireResult.js
@@ -8,19 +8,28 @@ import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { updateInterviewById, getInterviewById } from '../../Utils/Axios';
 
 import ScoreBar from '../EntireResult/ScoreBar';
 import Graph from './Graph';
 
 import html2canvas from "html2canvas";
 
-const EntireResult = ({handleLast, handleNext, handleClose, dates, bigs, page, career}) => {
+const EntireResult = ({id, handleLast, handleNext, handleClose, dates, bigs, page, industry}) => {
 
     const OCEAN = ["OPENESS", "CONSCIENTIOUS", "EXTRAVERSION", "AGREEABLENESS","NEUROTICISM"];
     
-
     const [openNote, setOpenNOte] = useState(false);
     const [readOnly, setReadOnly] = useState(true);
+    const [note, setNote] = useState('')
+
+    // useEffect(() => {
+    //     getInterviewById(interview.id).then((res) => {
+    //         setNote(res)
+    //     }).catch((err) => {
+    //         console.log(err)
+    //     })
+    // }, [])
 
     const handleOnClickAddNote = () => {
         setOpenNOte(true);
@@ -35,7 +44,17 @@ const EntireResult = ({handleLast, handleNext, handleClose, dates, bigs, page, c
     }
     const editNoteOff = () => {
         setReadOnly(true)
+        // updateInterviewById(interview.id, null, null, null, null, null, note)
+        //     .then()
+        //     .catch((err) => {
+        //         console.log(err)
+        //     })
     }
+
+    const handleChange = (event) => {
+        setNote(event.target.value);
+    };
+    
 
     const exportRef = useRef();
 
@@ -109,6 +128,7 @@ const EntireResult = ({handleLast, handleNext, handleClose, dates, bigs, page, c
                         backgroundColor: '#FFFFFF', 
                         borderRadius: '30px',
                         fontWeight: 'bold',
+                        pb: 2
                     }}
                     container
                     direction="column"
@@ -135,23 +155,25 @@ const EntireResult = ({handleLast, handleNext, handleClose, dates, bigs, page, c
                         marginTop='0.5vh'
                     >
                         <Graph 
-                            career = {career}
+                            industry = {industry}
                             big = {bigs[page]}                        
                         />
                     </Grid>
                      
-                    { openNote ? (<>
+                    { openNote ? (
+                        <>
                         <Grid
                             display="flex" 
                             justifyContent="space-around"
                             marginTop='2vh'
                         >
+                            <Button onClick={() => exportAsImage(exportRef.current, "test")} color="black">EXPORT</Button>
                             { readOnly ? (
-                                <Button onClick={editNoteOn}>EDIT</Button>
+                                <Button onClick={editNoteOn} color="black">EDIT</Button>
                             ):(
-                                <Button onClick={editNoteOff}>SAVE</Button>
+                                <Button onClick={editNoteOff} color="black">SAVE</Button>
                             )}
-                            <Button onClick={handleOnClickDelNote}>DELETE</Button>
+                            <Button onClick={handleOnClickDelNote} color="warning">DELETE</Button>
                         </Grid>
                         <Grid
                             display="flex" 
@@ -173,13 +195,16 @@ const EntireResult = ({handleLast, handleNext, handleClose, dates, bigs, page, c
                                     multiline
                                     variant="outlined"
                                     defaultValue=""
+                                    value={note}
+                                    onChange={handleChange}
                                     InputProps={{
                                         readOnly: readOnly,
                                     }}
                                 />
                             </Box>
                         </Grid>
-                        </>):(
+                        </>
+                        ):(
                         <Grid 
                             display="flex" 
                             justifyContent="space-around"
@@ -189,10 +214,6 @@ const EntireResult = ({handleLast, handleNext, handleClose, dates, bigs, page, c
                             <Button onClick={handleOnClickAddNote}>ADD NOTE</Button>
                         </Grid>
                     )}
-                    <Button 
-                        onClick={() => exportAsImage(exportRef.current, "test")}
-                        style = {{marginBottom: '2vh'}}
-                    >EXPORT AS IMAGE</Button>
                 </Grid>
             </Grid>
         </>
--- frontend/src/Components/EntireResult/Graph.js
+++ frontend/src/Components/EntireResult/Graph.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useCallback, useEffect } from 'react'
 import Grid from '@mui/material/Grid';
-// import Button from '@mui/material/Button';
+import Button from '@mui/material/Button';
 
 import PropTypes from 'prop-types';
 import Tabs from '@mui/material/Tabs';
@@ -40,103 +40,73 @@ function a11yProps(index) {
   };
 }
 
-const Graph = (career, big) => {
+const Graph = (industry, big5) => {
+  // for tabs
+  const [value, setValue] = useState(0);
+  const handleChange = (event, newValue) => {
+      setValue(newValue);
+  };
 
-    const [value, setValue] = useState(0);
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-    };
-
-    const str = JSON.stringify(big)
-    console.log(str);
-
-    return (
-        <>
-            <Typography variant="caption"> Tap to see distribution</Typography>
+  return (
+    <>
+      {/* <Typography variant="caption"> Tap to see distribution</Typography> */}
+      <Grid
+        container
+        direction="row"
+        justifyContent="start"
+        alignItems="center"
+        width='80vw'
+        marginLeft = '5vw'
+        marginRight = '5vw'
+      >
+        <Box width='100vw' style={{ overflow: 'hidden' }}>
+          <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            variant="scrollable"
+            scrollButtons="auto"
+            sx={{pr: 4}}
+          >
+            <Tab label="OPENESS" {...a11yProps(0)} />
+            <Tab label="CONSCIENTIOUS" {...a11yProps(1)} />
+            <Tab label="EXTRAVERSION" {...a11yProps(2)} />
+            <Tab label="AGREEABLENESS" {...a11yProps(3)} />
+            <Tab label="NEUROTICISM" {...a11yProps(4)}/>
+          </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
             <Grid
-                container
-                direction="row"
-                justifyContent="start"
-                alignItems="center"
-                width='80vw'
-                marginLeft = '5vw'
-                marginRight = '5vw'   
-                
+              container
+              direction="column"
+              justifyContent="start"
+              alignItems="center"
+              style={{
+                background: "#DBDBDB"
+              }}
+              width="70vw"
+              paddingLeft="8vw"
+              paddingRight="8vw"
+              borderRadius='16px'
             >
-                <Box width='100vw' style={{ overflow: 'hidden' }}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                    <Tabs 
-                        value={value} 
-                        onChange={handleChange}
-                        variant="scrollable"
-                        scrollButtons="auto"
-                        aria-label="scrollable auto tabs example"
-                        sx={{pr: 4}}
-                    >
-                        <Tab label="OPENESS" {...a11yProps(0)} />
-                        <Tab label="CONSCIENTIOUS" {...a11yProps(1)} />
-                        <Tab label="EXTRAVERSION" {...a11yProps(2)} />
-                        <Tab label="AGREEABLENESS" {...a11yProps(3)} />
-                        <Tab label="NEUROTICISM" {...a11yProps(4)}/>
-                    </Tabs>
-                    </Box>
-                    <TabPanel value={value} index={0}>
-                        <Grid
-                            container
-                            direction="column"
-                            justifyContent="start"
-                            alignItems="center"
-                            style={{
-                                background: "#DBDBDB"
-                            }}
-                            width="70vw"
-                            paddingLeft="8vw"
-                            paddingRight="8vw"
-                            borderRadius='16px'  
-                        >
-                            <p>This is graph for 'O'</p>
-                        </Grid>
-                    </TabPanel>
-                    <TabPanel value={value} index={1}>
-                        Item Two
-                    </TabPanel>
-                    <TabPanel value={value} index={2}>
-                        Item Three
-                    </TabPanel>
-                    <TabPanel value={value} index={3}>
-                        Item Four
-                    </TabPanel>
-                    <TabPanel value={value} index={4}>
-                        Item Five
-                    </TabPanel>
-                </Box>
+              <p>This is graph for 'O'</p>
             </Grid>
-            {/* <Grid
-                container
-                direction="column"
-                justifyContent="start"
-                alignItems="center"
-                width='80vw'
-                marginTop='0.5vh'  
-            >
-                <Grid
-                    container
-                    direction="column"
-                    justifyContent="start"
-                    alignItems="center"
-                    style={{
-                        background: "#DBDBDB"
-                    }}
-                    width="70vw"
-                    paddingLeft="8vw"
-                    paddingRight="8vw"
-                    borderRadius='16px'  
-                >
-                    <p>This is the graph.</p>
-                </Grid>  
-            </Grid> */}
-            
-        </>
-    )
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            Item Two
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            Item Three
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            Item Four
+          </TabPanel>
+          <TabPanel value={value} index={4}>
+            Item Five
+          </TabPanel>
+        </Box>
+      </Grid>
+    </>
+  )
 }
 export default Graph
