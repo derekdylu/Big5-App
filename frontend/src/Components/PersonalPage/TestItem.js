fb6cef6fce08c81acb329c3cd6318917553d90a9
Isabelle1007 <58721990+Isabelle1007@users.noreply.github.com>
2022-12-15 23:04:43 +0800

    minor change of page refresh

--- frontend/src/Components/PersonalPage/TestItem.js
+++ frontend/src/Components/PersonalPage/TestItem.js
@@ -10,12 +10,13 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import CircularProgress from '@mui/material/CircularProgress';
 
 import ScoreBar from '../EntireResult/ScoreBar';
 import PureBar from '../EntireResult/PureBar'
 import EntireResult from '../EntireResult/EntireResult';
 
-import { getInterviewsByUserId } from '../../Utils/Axios';
+import { getInterviewById } from '../../Utils/Axios';
 
 const Transition = React.forwardRef(
     function Transition(props, ref) {
@@ -25,16 +26,16 @@ const c = ['#4FC1E8', '#AC92EB', '#FFCE54', '#A0D568', '#ED5564']
 
 const TestItem = ({userId, interview, interviews, setInterviews}) => {
 
-    // useEffect(() => {
-    //     console.log('render')
-    //     navigate('/');
-    //     // getInterviewsByUserId(userId).then((res) => {
-    //     //     // interviews = interviews
-    //     //     console.log('res', res)
-    //     // }).catch((err) => {
-    //     //     console.log(err)
-    //     // })
-    // }, [])
+    useEffect(() => {
+        getInterviewById(interview._id).then((res) => {
+            // console.log('score', res.score)
+            if(res.score != 50){
+                setLoading(false)
+            }
+        }).catch((err) => {
+            console.log(err)
+        })
+    }, [])
 
     const itemObj = {
         backgroundColor: '#FFFFFF',
@@ -43,9 +44,11 @@ const TestItem = ({userId, interview, interviews, setInterviews}) => {
         marginTop: '1vh',
         marginBottom: '1vh',
         paddingLeft: '8vw',
+        cursor: 'pointer'
     };
     
     const [open, setOpen] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const changeDateFormat = (timestamp) => {
         var result = new Date(parseInt(timestamp))
@@ -66,24 +69,36 @@ const TestItem = ({userId, interview, interviews, setInterviews}) => {
                 <Grid 
                     container spacing={2}
                     onClick = {() => {setOpen(true)}}
-                    cursor = 'pointer'
+                    marginTop = '0px'
                 >
                     <Grid item xs={7}>
-                        <p style = {{fontWeight: '700', marginBottom: '-0.5em'}}>{interview.topic}</p>
+                        <p style = {{fontWeight: '700',marginTop: '0em', marginBottom: '0em'}}>{interview.topic}</p>
                         <p style = {{color: 'gray', fontWeight: '500'}}>{changeDateFormat(interview.timestamp)}</p>
                     </Grid>
-                    <Grid 
-                        item xs={4}
-                        container
-                        direction="column"
-                        justifyContent="center"
-                    >
-                        {
-                            interview.big.map((b, id) => 
-                                <PureBar pro = {b} color = {c[id]}/>
-                            )
-                        }
-                    </Grid>
+                    { loading ? 
+                        (<Grid 
+                            item xs={4}
+                            container
+                            direction="column"
+                            justifyContent="center"
+                            marginTop= '-1vh'
+                        >
+                            <CircularProgress/>
+                        </Grid>)
+                        :
+                        (<Grid 
+                            item xs={4}
+                            container
+                            direction="column"
+                            justifyContent="center"
+                        >
+                            {
+                                interview.big.map((b, id) => 
+                                    <PureBar pro = {b} color = {c[id]}/>
+                                )
+                            }
+                        </Grid>)
+                    }
                 </Grid>
             </div>
             <Dialog
